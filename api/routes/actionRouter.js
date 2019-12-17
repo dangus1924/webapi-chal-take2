e9c2b3478b50a2d05f3260966a2d426976f6287f
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2019-12-16 20:05:53 -0500

    added three action routes now testing now importing to server to run a test

--- api/routes/actionRouter.js
+++ api/routes/actionRouter.js
@@ -1,5 +1,33 @@
 const express = require('express');
 const actions = require('../data/helpers/actionModel');
+const { validateAction, validateActionId } = require('../middleware/validate');
+const router = express.Router({ mergeParams: true });
 
+router.get("/", (req, res, next) => {
+    actions.get()
+      .then(actions => {
+        res.status(200).json(actions)
+      })
+      .catch(error => {
+        next(error)
+      })
+  })
+  
+  router.get("/:id", validateActionId(), (req, res, next) => {
+    return res.json(req.action)
+  })
+  
+  router.post("/", validateActionId(), validateAction(), (req, res, next) => {
+    const { id } = req.params
+    const { description, notes } = req.body
+  
+    actions.insert({ project_id: id, description, notes })
+      .then(action => {
+        res.status(201).json(action)
+      })
+      .catch(error => {
+        next(error)
+      })
+  })
 
-
+  module.exports = router;
