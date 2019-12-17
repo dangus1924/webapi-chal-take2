d87f41965a1921559495b34dacfa03b8da3bc2b2
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2019-12-16 20:39:00 -0500

    created project rotues

--- api/routes/actionRouter.js
+++ api/routes/actionRouter.js
@@ -3,6 +3,7 @@ const actions = require('../data/helpers/actionModel');
 const { validateAction, validateActionId } = require('../middleware/validate');
 const router = express.Router({ mergeParams: true });
 
+
 router.get("/", (req, res, next) => {
     actions.get()
       .then(actions => {
@@ -24,6 +25,29 @@ router.get("/", (req, res, next) => {
     actions.insert({ project_id: id, description, notes })
       .then(action => {
         res.status(201).json(action)
+      })
+      .catch(error => {
+        next(error)
+      })
+  })
+
+  router.put("/:id", validateActionId(), validateAction(), (req, res, next) => {
+    const { id } = req.action
+    const { description, notes } = req.body
+  
+    actions.update(id, { description, notes })
+      .then(action => {
+        res.status(200).json(action)
+      })
+      .catch(error => {
+        next(error)
+      })
+  })
+  
+  router.delete("/:id", validateActionId(), (req, res, next) => {
+    actions.remove(req.action.id)
+      .then(action => {
+        return res.status(200).json(action)
       })
       .catch(error => {
         next(error)
--- api/server.js
+++ api/server.js
@@ -1,20 +1,22 @@
 const express = require('express');
-const logger = require('./middleware/logger')
-const helmet = require('helmet')
+const logger = require('./middleware/logger');
+const helmet = require('helmet');
 
+
+const actionRouter = require('./routes/actionRouter');
+const projectRouter = require('./routes/projectRouter');
 
 const server = express();
 
-const actionRouter = require('./routes/actionRouter')
 
-
-server.use(express.json(), helmet(), logger)
+server.use(express.json(), helmet(), logger);
 
 server.get('/', (req, res) => {
     res.send(`<h1>Welcome to the API app.</h1>`)
 });
 
-server.use('/.api/actions', actionRouter)
+server.use('/api/actions', actionRouter)
+server.use('./api/projects', projectRouter)
 
 server.use((req, res) => {
     return res.status(404).json({ message: "Wrong turn this page does not exist. Try again!"})
