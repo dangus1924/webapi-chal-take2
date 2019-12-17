7e6b281511bee26a38c24a89bbfc5b084f7bee53
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2019-12-16 20:09:28 -0500

    added the action to server and created the error handler now debugging error

--- api/server.js
+++ api/server.js
@@ -5,9 +5,26 @@ const helmet = require('helmet')
 
 const server = express();
 
+const actionRouter = require('./routes/actionRouter')
+
 
 server.use(express.json(), helmet(), logger)
 
 server.get('/', (req, res) => {
     res.send(`<h1>Welcome to the API app.</h1>`)
 });
+
+server.use('/.api/actions', actionRouter)
+
+server.use((req, res) => {
+    return res.status(404).json({ message: "Wrong turn this page does not exist. Try again!"})
+  })
+  
+  server.use((err, req, res, next) => {
+    console.log(err)
+    res.status(500).json({
+      message: "An error occurred, please try again later."
+    })
+  })
+  
+  module.exports = server;
